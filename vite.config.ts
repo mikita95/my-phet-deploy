a2821f27b54b88c1eb19eff633b2759775dc00a5
Nikita Markovnikov <niklemar@amazon.com>
2025-04-01 22:07:45 +0200

    fix pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,10 @@
 import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 export default defineConfig({
     plugins: [react()],
-    root: '.', // default, but makes structure clear
-    publicDir: 'public', // default, just being explicit
     build: {
         outDir: 'dist',
         emptyOutDir: true,
-        rollupOptions: {
-            input: path.resolve(__dirname, 'public/index.html') // ✅ use the correct full path
-        }
     }
 });
